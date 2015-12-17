a830593f7c4949cd0da63f25d68bfa4782637ba9
danielboyle <danielboyle111@gmail.com>
2015-12-16 20:03:39 -0500

    Add little more touch up

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-  var answer = Math.floor(Math.random() * 101),
+  var random_number = Math.floor(Math.random() * 101),
+      answer = random_number,
       guesses = 0;
 
   $("form").on("submit", function(e) {
@@ -9,21 +10,25 @@ $(document).ready(function() {
         message = "";
 
     guesses++;
-    if (guess > answer) {
+    if (guess < 0 || guess > 100 || isNaN(guess)) {
+      message = "Please enter a valid number.";
+    } else if (guess > answer) {
       message = "My number is lower than " + guess;
     } else if (guess < answer) {
       message = "My number is higher than " + guess;
     } else {
-      message = "You win! It took you " + guesses + " guesses to get it right!";
+      message = "You win! My number is " + answer + "!<br>" + 
+      "It took you " + guesses + " guesses to get it right!";
     }
-    $("p").text(message);
+    $("p").html(message);
+    $("#guess").val("");
   });
 
   $("a").on("click", function(e) {
     e.preventDefault();
-    answer = Math.floor(Math.random() * 101);
+    answer = random_number;
     guesses = 0;
-    $("p").text("Guess a number from 1 to 100");
+    $("p").html("Guess a number from 1 to 100");
   });
 
 });
